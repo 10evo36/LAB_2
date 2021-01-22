a43ba81e957502cf34ecd986dd31bbcddc227aa7
10evo36 <75244122+10evo36@users.noreply.github.com>
2021-01-22 19:38:38 +0300

    Update individual.py

--- individual.py
+++ individual.py
@@ -1,6 +1,9 @@
-Speed_1 =int(input("Скорость первого автомобиря"))
-Speed_2 =int(input("Скорость второго автомобиля"))
-All =int(input("Расстояние"))
-Speed_Sum = Speed_1+Speed_2
-Time = All/Speed_Sum
-print("Автомобили встретятся через", int(Time), "час(а)")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    Speed_1 = int(input("Скорость первого автомобиря"))
+    Speed_2 = int(input("Скорость второго автомобиля"))
+    All = int(input("Расстояние"))
+    Speed_Sum = Speed_1+Speed_2
+    Time = (All/Speed_Sum)
+    print("Автомобили встретятся через", int(Time), "час(а)")
