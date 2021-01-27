32bfbc7a10604a54a2c9530fcdeda845c182febf
10evo36 <75244122+10evo36@users.noreply.github.com>
2021-01-27 12:31:28 +0300

    Update User.py

--- User.py
+++ User.py
@@ -1,6 +1,11 @@
-User_name = input("What is your name? ")
-User_age = input("How old are you? ")
-User_liveplace = input("Where are you live? ")
-print ( "This is", User_name)
-print ( "It is" , User_age)
-print ( "(S)he live in", User_liveplace)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    # Введём личные данные
+    User_name = input("What is your name? ")
+    User_age = input("How old are you? ")
+    User_live = input("Where are you live? ")
+    # Выведем личные данные
+    print("This is", User_name)
+    print("It is", User_age)
+    print("(S)he live in", User_live)
