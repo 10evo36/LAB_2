91197381d1a32f3033ae963cc54e18e0b2c7a764
10evo36 <75244122+10evo36@users.noreply.github.com>
2021-01-27 12:35:44 +0300

    Update User.py

--- User.py
+++ User.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 if __name__ == '__main__':
+    
     # Введём личные данные
     User_name = input("What is your name? ")
     User_age = input("How old are you? ")
     User_live = input("Where are you live? ")
+    
     # Выведем личные данные
     print("This is", User_name)
     print("It is", User_age)
