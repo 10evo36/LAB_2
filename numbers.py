dad48c1c6fed2220439f1f3d63928067b61684da
10evo36 <75244122+10evo36@users.noreply.github.com>
2021-01-28 13:55:58 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 if __name__ == '__main__':
-    # Ввести 4 числа
-    Number_one = float(input("Введите первое число"))
-    Number_two = float(input("Введите второе число"))
-    Number_three = float(input("Введите третье число"))
-    Number_four = float(input("Введите четвертое число"))
-    # Посчитать сумму первых двух чисел и вторых двух, затем разделить сумму первых чисел на сумму вторых
-    Sum_1 = Number_one + Number_two
-    Sum_2 = Number_three + Number_four
-    Div = Sum_1 / Sum_2
-    # Вывести результат с двумя числами после запятой
-    print("% .2f" % Div)
+    # Ввести 4 числа.
+    number1 = float(input("Введите первое число"))
+    number2 = float(input("Введите второе число"))
+    number3 = float(input("Введите третье число"))
+    number4 = float(input("Введите четвертое число"))
+
+    # Посчитать сумму первых двух чисел и вторых двух, затем разделить
+    # сумму первых чисел на сумму вторых.
+    sum1 = number1 + number2
+    sum2 = number3 + number4
+    div = sum1 / sum2
+
+    # Вывести результат с двумя числами после запятой.
+    print("% .2f" % div)
